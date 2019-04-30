ccad087d85c3d0fe3a55baa33b3c95d97f2669f3
Taylor Lyles <hawks93187@gmail.com>
2019-04-30 10:48:25 -0700

    Playground file saving fun

--- FileManager.playground/Contents.swift
+++ FileManager.playground/Contents.swift
@@ -5,3 +5,47 @@ print(str)
 
 // Documents directory
 
+let fileManager = FileManager.default
+let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+// /Users/taylorlyles/Documents/
+
+// Create a filename
+let location = documentsDirectory.appendingPathComponent("star.plist")
+
+// /Users/taylorlyles/Documents/star.plist
+print(location.path)
+
+struct Star: Codable {
+    var name: String
+    var distance: Double
+}
+
+let star = Star(name: "Proxima Centauri", distance: 4.24)
+
+// Save to disk
+
+let encoder = PropertyListEncoder()
+do {
+    let data = try encoder.encode(star)
+    print(data)
+    print(data as NSData)
+    try data.write(to: location)
+} catch {
+    print("Error saving to disk: \(error)")
+}
+
+// Load from disk
+
+let decoder = PropertyListDecoder()
+do {
+    let data = try Data(contentsOf: location)
+    let loadedStar = try decoder.decode(Star.self, from: data)
+    print("Star: \(loadedStar)")
+} catch {
+    print("Error saving to disk: \(error)")
+}
+
+// Inspect Finder/Terminal
+
+
